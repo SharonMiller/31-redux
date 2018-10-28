b18dd075f0f1c75e95b63b3e4c682e61c42029f6
Sharon Miller <sharon@sharonmillercreative.com>
2018-10-28 15:23:30 -0700

    update link to import all reducers, fixed bugs, form updates

--- src/component/category-item/index.js
+++ src/component/category-item/index.js
@@ -1,7 +1,9 @@
 // containing indexForm component this is the array 
 
 import React from 'react';
-import CategoryForm from '../category-form'
+import CategoryForm from '../category-form';
+import ExpenseForm from '../expense-form/index';
+import ExpenseItem from '../expense-item/index';
 
 
 class CategoryItem extends React.Component {
@@ -10,40 +12,69 @@ class CategoryItem extends React.Component {
 
     this.state = {
       categoryEdit: false,
+      expenseEdit: false,
+      expenseCreate: false,
     }
 
 
     this.handleDelete = this.handleDelete.bind(this)
     this.toggleCategoryEdit = this.toggleCategoryEdit.bind(this)
+    this.toggleExpenseCreate = this.toggleExpenseCreate.bind(this)
   }
-  handleDelete(e) {
-    this.props.destroy(this.props.category);
+
+  handleDelete(e, category) {
+    this.props.actions.categoryDelete(category)
   }
 
   toggleCategoryEdit() {
     this.setState({ categoryEdit: !this.state.categoryEdit })
   }
+
+  toggleExpenseCreate() {
+    this.setState({ expenseCreate: !this.state.expenseCreate })
+  }
+
   render() {
     return (
-      <section onDoubleClick={this.toggleCategoryEdit}>
+      <section onDoubleClick={this.toggleCategoryEdit} className="budget-item">
+        <h4>Category</h4>
         {this.state.categoryEdit ?
           <CategoryForm
             buttonText="update category"
-            onComplete={this.props.onComplete}
+            onComplete={this.props.actions.categoryUpdate}
             category={this.props.category}
             toggle={this.toggleCategoryEdit} /> :
           <div>
-            <p>{this.props.category.title}</p>
-            <p>${this.props.category.budget}</p>
-            <button onClick={this.handleDelete}> X </button>
+            <button onClick={(ev) => this.handleDelete(ev, this.props.category)}>x</button>
+            <button onClick={this.toggleExpenseCreate}>+ expense</button>
+            <p>{this.props.category.title}: <span>${this.props.category.budget}</span></p>
+            {this.state.expenseCreate ?
+              <ExpenseForm
+                onComplete={this.props.actions.expenseCreate}
+                buttonText="create expense"
+                category={this.props.category}
+                toggle={this.toggleExpenseCreate} /> :
+              undefined
+            }
 
-
-
+            {this.props.expenses.length ?
+              <aside className="expense-list">
+                <h4>Expenses</h4>
+                {this.props.expenses.map(expense => {
+                  return <ExpenseItem
+                    key={expense.id}
+                    expense={expense}
+                    category={this.props.category}
+                    actions={this.props.actions} />
+                })}
+              </aside> :
+              undefined
+            }
           </div>
         }
       </section>
     )
   }
-};
+}
 
-export default CategoryItem;
+export default CategoryItem
