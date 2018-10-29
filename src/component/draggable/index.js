74af42b8186efbd36ba359f97c1fb2f2568ea78f
Sharon Miller <sharon@sharonmillercreative.com>
2018-10-28 20:15:29 -0700

    dropzone and draggable complete still getting error

---
+++ src/component/draggable/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Draggable extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleDragStart = this.handleDragStart.bind(this);
+  };
+
+  handleDragStart(e) {
+    e.preventDefault()
+    let jsonItem = JSON.stringify(this.props.dataTransferItem);
+    e.dataTransfer.setData('application/json', jsonItem);
+
+  }
+
+  render() {
+    return (
+      <div draggable className="draggable" onDragStart={this.handleDragStart}>
+        {this.props.children}
+      </div>
+    );
+  }
+};
+
+export default Draggable;
